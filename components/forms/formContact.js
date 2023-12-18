42e3b346bffe9b52d61a981434803773af547040
loveheenavasu <trivediheenalove@gmail.com>
2023-12-18 15:09:39 +0530

    add keap form in contact page

--- components/forms/formContact.js
+++ components/forms/formContact.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import { Section, Container, Flex, FlexMobileOpp } from "../layoutComponents";
@@ -38,128 +38,44 @@ const Socials = styled.div`
   }
 `;
 
-export default function FormContact(props) {
-  const [properties, setProperties] = useState({
-    keep_up_with_us: "",
-    Session_wanted_for: "",
-    first_Name: "",
-    last_name: "",
-  });
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    note: "",
-    properties: [
-      {
-        name: "first_name",
-        value: "",
-      },
-      {
-        name: "last_name",
-        value: "",
-      },
-      {
-        name: "keep_up_with_us",
-        value: "",
-      },
-      {
-        name: "Session_wanted_for",
-        value: "",
-      },
-    ],
-  });
-  useEffect(() => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      name: properties?.first_Name + " " + properties.last_name,
-      properties: [
-        {
-          name: "first_name",
-          value: properties?.first_Name,
-        },
-        {
-          name: "last_name",
-          value: properties?.last_name,
-        },
-        {
-          name: "keep_up_with_us",
-          value: properties?.keep_up_with_us,
-        },
-        {
-          name: "Session_wanted_for",
-          value: properties?.Session_wanted_for,
-        },
-      ],
-    }));
-  }, [properties]);
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        "https://markl.sendly.co.uk/api/site/contacts",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer w74fd560ced9a463990c64c52592be364", // Set the content type to JSON
-            // Add any other headers if needed
-          },
-          body: JSON.stringify(formData), // Convert your data to a JSON string
-        }
-      );
-      if (response.status === 200) {
-        console.log("dat saved");
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+export default function FormContest(props) {
   return (
     <Section>
       <Container className="spacing">
         <Text>
-          <h3 className="subhead caps accent">
-            your complimentary planning session is the first step on your{" "}
-            <span className="italics">transformative journey!</span>
-          </h3>
+          <h3 className="subhead caps accent">enter to win!</h3>
         </Text>
         <Flex className="spacing-lg">
           {/* flex item 1 */}
           <FormWrapper>
             <form
               acceptCharset="UTF-8"
-              // action="https://markl.sendly.co.uk/api/site/contacts"
-              // method="POST"
+              action="https://im322.infusionsoft.com/app/form/process/cdd5e6d034dc6adfbcbaa93a74ea5667"
+              className="infusion-form"
+              id="inf_form_cdd5e6d034dc6adfbcbaa93a74ea5667"
+              method="POST"
             >
-              {/* <input
-                name="send_form_xid"
+              <input
+                name="inf_form_xid"
                 type="hidden"
-                defaultValue="874dc5be8f3ee257d63dd0f53f29707e"
+                defaultValue="cdd5e6d034dc6adfbcbaa93a74ea5667"
               />
               <input
-                name="send_form_name"
+                name="inf_form_name"
                 type="hidden"
-                defaultValue="10. Marketing - Free &#a;Consultation Form (471)"
+                defaultValue="10. Marketing - Enter &#a;to Win Form (461)"
               />
               <input
                 name="infusionsoft_version"
                 type="hidden"
                 defaultValue="1.70.0.503005"
-              /> */}
+              />
               <div>
                 <div>
                   <div className="title">
                     <div className="title" contentid="title">
                       <div>
-                        <span data-mce-mark={1}>
-                          <strong>
-                            <span data-mce-mark={1}>
-                              Request a Free Consultation
-                            </span>
-                          </strong>
-                        </span>
+                        <span>Enter To Win</span>
                       </div>
                     </div>
                   </div>
@@ -169,47 +85,29 @@ export default function FormContact(props) {
                 <div>&nbsp;</div>
               </div>
               <div className="spacing">
-                <div className="field">
-                  <Label htmlFor="firstName">First Name *</Label>
+                <div className="infusion-field">
+                  <Label htmlFor="inf_field_FirstName">First Name *</Label>
                   <Input
-                    id="firstName"
-                    name="firstName"
+                    id="inf_field_FirstName"
+                    name="inf_field_FirstName"
                     placeholder="First Name *"
-                    value={properties.first_Name}
-                    onChange={(e) =>
-                      setProperties({
-                        ...properties,
-                        first_Name: e.target.value,
-                      })
-                    }
                     type="text"
                   />
                 </div>
-                <div className="field">
-                  <Label htmlFor="lastname">Last Name *</Label>
+                <div className="infusion-field">
+                  <Label htmlFor="inf_field_LastName">Last Name *</Label>
                   <Input
-                    id="Lastname"
-                    name="lastname"
-                    value={properties.last_name}
-                    onChange={(e) =>
-                      setProperties({
-                        ...properties,
-                        last_name: e.target.value,
-                      })
-                    }
+                    id="inf_field_LastName"
+                    name="inf_field_LastName"
                     placeholder="Last Name *"
                     type="text"
                   />
                 </div>
                 <div className="infusion-field">
-                  <Label htmlFor="email">Email *</Label>
+                  <Label htmlFor="inf_field_Email">Email *</Label>
                   <Input
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={(e) =>
-                      setFormData({ ...formData, email: e.target.value })
-                    }
+                    id="inf_field_Email"
+                    name="inf_field_Email"
                     placeholder="Email *"
                     type="text"
                   />
@@ -218,11 +116,7 @@ export default function FormContact(props) {
                   <Label htmlFor="inf_field_Phone1">Phone Number *</Label>
                   <Input
                     id="inf_field_Phone1"
-                    value={formData.phone}
-                    onChange={(e) =>
-                      setFormData({ ...formData, phone: e.target.value })
-                    }
-                    name="phone"
+                    name="inf_field_Phone1"
                     placeholder="Phone Number *"
                     type="text"
                   />
@@ -239,81 +133,41 @@ export default function FormContact(props) {
                 </div>
               </div>
               <div className="infusion-field">
-                <Label htmlFor="inf_option_KeepUpWithUs">
-                  Keep Up With Us *
+                <Label htmlFor="inf_option_Keepupwithus">
+                  Keep up with us *
                 </Label>
                 <div className="infusion-radio">
                   <div className="options-container">
                     <span className="infusion-option">
                       <>
-                        <Label htmlFor="inf_option_KeepUpWithUs_701">
+                        <Label htmlFor="inf_option_Keepupwithus_709">
                           Yes, I would love to get updates on contests, photo
                           tips, specials, and more.
                         </Label>
                         <Input
-                          id="inf_option_KeepUpWithUs_701"
-                          name="inf_option_KeepUpWithUs"
-                          value={
-                            "Yes, I would love to get updates on contests, phototips, specials, and more."
-                          }
-                          onChange={(e) =>
-                            setProperties({
-                              ...properties,
-                              keep_up_with_us: e.target.value,
-                            })
-                          }
-                          type="radio"
+                          id="inf_option_Keepupwithus_709"
+                          name="inf_option_Keepupwithus"
                           className="radio"
-                          defaultValue={701}
+                          type="radio"
+                          defaultValue={709}
                         />
                       </>
                     </span>
                     <span className="infusion-option">
                       <>
-                        <Label htmlFor="inf_option_KeepUpWithUs_703">
+                        <Label htmlFor="inf_option_Keepupwithus_711">
                           No, I don't want to get updates
                         </Label>
                         <Input
-                          id="inf_option_KeepUpWithUs_703"
-                          name="inf_option_KeepUpWithUs"
-                          value={"No, I don't want to get updates"}
-                          onChange={(e) =>
-                            setProperties({
-                              ...properties,
-                              keep_up_with_us: e.target.value,
-                            })
-                          }
-                          type="radio"
+                          id="inf_option_Keepupwithus_711"
                           className="radio"
-                          defaultValue={703}
+                          name="inf_option_Keepupwithus"
+                          type="radio"
+                          defaultValue={711}
                         />
                       </>
                     </span>
                   </div>
-                </div>
-              </div>
-              <div className="infusion-field">
-                <Label htmlFor="inf_custom_SessionWantedFor">
-                  Session Wanted For:
-                </Label>
-                <div className="infusion-field-input-container">
-                  <Select
-                    id="inf_custom_SessionWantedFor"
-                    name="inf_custom_SessionWantedFor"
-                    onChange={(e) => {
-                      setProperties({
-                        ...properties,
-                        Session_wanted_for: e.target.value,
-                      });
-                    }}
-                  >
-                    <option value="Please select one">Please select one</option>
-                    <option value="Gift">Gift</option>
-                    <option value="Adventure">Adventure</option>
-                    <option value="Empowerment">Empowerment</option>
-                    <option value="Mix">Mix</option>
-                    <option value="Not Sure">Not Sure</option>
-                  </Select>
                 </div>
               </div>
               <div>
@@ -322,9 +176,6 @@ export default function FormContact(props) {
               <div className="infusion-field">
                 <Label htmlFor="inf_custom_Message">Message *</Label>
                 <TextArea
-                  onChange={(e) => {
-                    setFormData({ ...formData, note: e.target.value });
-                  }}
                   cols={24}
                   id="inf_custom_Message"
                   name="inf_custom_Message"
@@ -339,12 +190,34 @@ export default function FormContact(props) {
               <div className="infusion-submit">
                 <button
                   className="infusion-recaptcha"
-                  id="recaptcha_874dc5be8f3ee257d63dd0f53f29707e"
-                  // type="submit"
-                  onClick={handleFormSubmit}
+                  id="recaptcha_cdd5e6d034dc6adfbcbaa93a74ea5667"
+                  type="submit"
                 >
                   Submit
                 </button>
+              </div>
+              <div>
+                <div>&nbsp;</div>
+              </div>
+              <div>
+                <div className="text">
+                  <div className="text" contentid="paragraph">
+                    <div>
+                      <span>
+                        <em>
+                          *View Contest{" "}
+                          <a
+                            href="https://www.innerspiritphoto.com/contest-rules/"
+                            nottracked="true"
+                            shape="rect"
+                          >
+                            Rules &amp; Regulations
+                          </a>
+                        </em>
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </form>
           </FormWrapper>
